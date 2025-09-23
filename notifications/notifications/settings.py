0fca5c99d8564ba1907f35de8d03eddc297cbe9b
Anton <bo1ko.ant@yandex.com>
2025-09-23 17:15:07 +0300

    added swagger

--- notifications/notifications/settings.py
+++ notifications/notifications/settings.py
@@ -29,8 +29,19 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'drf_spectacular',
     'send_notifications',
 ]
+# DRF / Spectacular
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Notification Service API',
+    'DESCRIPTION': 'API for sending notifications via Email and Telegram',
+    'VERSION': '1.0.0',
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
